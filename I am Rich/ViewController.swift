c405c0761963df65b8bdea35431edb209179eba4
Dasha Ainslie <dasha.ainslie@gmail.com>
2021-03-16 12:22:08 +0000

    Initial

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dasha Ainslie on 16/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
